5d666039d6f603c4aa203a31091963216f96c530
Андрей Кумыков <aakumykov@yandex.ru>
2016-01-22 00:38:20 +0700

    CRUD для Пользователя

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,35 @@
 class UsersController < ApplicationController
   def create
+	  @user = User.new(name:params[:user][:name], email:params[:user][:email])
+	  @user.save!
+	  redirect_to users_path
   end
 
   def show
+	  @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+	  @user = User.find_by(id: params[:id])
   end
 
   def update
+	  @user = User.find_by(id: params[:id])
+	  @user.update_attributes!(name: params[:user][:name], email: params[:user][:email])
+	  redirect_to user_path(@user)
   end
 
   def destroy
+	  @user = User.find_by(id: params[:id])
+	  @user.destroy!
+	  redirect_to users_path
   end
 
   def index
+	  @list = User.all
   end
 
   def new
+	  @user = User.new
   end
 end
