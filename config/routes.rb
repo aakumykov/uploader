0fa98bb6ba79ad93ac058462088e4553afe820f4
Андрей Кумыков <aakumykov@yandex.ru>
2016-01-22 00:39:34 +0700

    Удалил приказчик welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/create'
-
-  get 'users/show'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/show'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'users/index'
-
   #get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
