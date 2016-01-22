a653397c854a242a7128cc201823bec43a0d0b38
Андрей Кумыков <aakumykov@yandex.ru>
2016-01-22 16:22:26 +0700

    Подготовил Прищепку, но проблемы с созданием
    Для проверки users#create вручную.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,44 @@
 class UsersController < ApplicationController
-  def create
-	  @user = User.new(name:params[:user][:name], email:params[:user][:email])
-	  @user.save!
-	  redirect_to users_path
-  end
+	def index
+		@list = User.all
+	end
+	
+	def new
+		@user = User.new
+	end
+	
+	def create
+		#@user = User.create!(user_params)
+		@user = User.create!(
+			name: params[:user][:name],
+			email: params[:user][:email],
+		)
+		redirect_to users_path
+	end
 
-  def show
-	  @user = User.find_by(id: params[:id])
-  end
+	def show
+		@user = User.find_by(id: params[:id])
+	end
 
-  def edit
-	  @user = User.find_by(id: params[:id])
-  end
+	def edit
+		@user = User.find_by(id: params[:id])
+	end
 
-  def update
-	  @user = User.find_by(id: params[:id])
-	  @user.update_attributes!(name: params[:user][:name], email: params[:user][:email])
-	  redirect_to user_path(@user)
-  end
+	def update
+		@user = User.find_by(id: params[:id])
+		@user.update_attributes!(user_params)
+		redirect_to user_path(@user)
+	end
 
-  def destroy
-	  @user = User.find_by(id: params[:id])
-	  @user.destroy!
-	  redirect_to users_path
-  end
+	def destroy
+		@user = User.find_by(id: params[:id])
+		@user.destroy!
+		redirect_to users_path
+	end
 
-  def index
-	  @list = User.all
-  end
 
-  def new
-	  @user = User.new
-  end
+	private
+		def user_params
+			params.require(:user).permit(:avatar)
+		end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+	has_attached_file :avatar, styles: { medium: "180x180>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
