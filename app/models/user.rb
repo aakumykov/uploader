a6012c46d580b3b980388796d844448bd414a354
Андрей Кумыков <aakumykov@yandex.ru>
2016-04-18 10:23:11 +0700

    fix: размеры прикреплённой картинки

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,57 +1,18 @@
 class User < ActiveRecord::Base
 	enum kind: { 'текст'=>'text', 'картинка'=>'picture', 'звук'=>'audio', 'видео'=>'video' }
 
-	has_attached_file :avatar
-	validates_attachment_content_type :avatar, 
-	content_type: [
-		#~ /.*/,
-		#~ 'audio/mpeg', 
-		#~ 'audio/x-mpeg', 
-		#~ 'audio/mp3', 
-		#~ 'audio/x-mp3', 
-		#~ 'audio/mpeg3', 
-		#~ 'audio/x-mpeg3', 
-		#~ 'audio/mpg', 
-		#~ 'audio/x-mpg', 
-		#~ 'audio/x-mpegaudio',
-		'audio/ogg',
-		'audio/x-ogg',
-		'video/ogg',
-		'video/x-ogg',
-		'application/ogg',
-		'application/x-ogg',
-		'application/msword',
-		'application/vnd.oasis.opendocument.text',
-		'application/pdf',
-		'application/x-bittorrent',
-		'audio/mpeg',
-		'audio/x-mpeg',
-		'application/mpeg',
-		'application/x-mpeg',
-		'audio/x-vorbis+ogg',
-		'application/ogg',
-		'video/ogg',
-	]
-
 	validates :name, presence: true
 
 	validates :kind, { presence:true }
 
-	#validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
-	#validates :avatar, attachment_presence: true
-	#validates :avatar, attachment_size: { less_than: 1.megabytes }
-	#validates :avatar, attachment_content_type: { content_type: 'application/vnd.oasis.opendocument.text' }
-	#validates :avatar, attachment_content_type: { content_type: 'application/msword' }
-	#~ validates(
-		#~ :avatar, 
-		#~ attachment_content_type: { 
-			#~ content_type: [
-				#~ 'image/jpeg',
-				#~ 'image/png',
-				#~ 'image/gif',
-				#~ 'audio/x-vorbis+ogg',
-				#~ 'audio/mpeg',
-			#~ ]
-		#~ }
-	#~ )
+	has_attached_file(
+		:avatar,
+		styles: {
+			medium: "120x120>",
+			thumb: "32x32>"
+		},
+		default_url: "/images/:style/missing.png"
+	)
+
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
